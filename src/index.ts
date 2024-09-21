4fb4f6b16667bc85410e552877b8398401d1a71f
jsnimda <7615255+jsnimda@users.noreply.github.com>
2024-09-21 23:29:07 +0800

    add name to default export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import type { FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
 
-const fastifyNoResponseValidation: FastifyPluginAsync = async (fastify) => {
+const plugin: FastifyPluginAsync = async (fastify) => {
   fastify.setSerializerCompiler(() => JSON.stringify);
 };
 
-export default fp(fastifyNoResponseValidation, {
+const fastifyNoResponseValidation = fp(plugin, {
   fastify: '>=4',
   name: 'fastify-no-response-validation'
 });
+
+export default fastifyNoResponseValidation;
